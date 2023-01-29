eb8c06ca53348223f1649aa54085369e987ab344
Muhammad Ahmer Tahir <82140155+webmentordev@users.noreply.github.com>
2023-01-29 17:06:42 +0500

    added favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,9 +6,14 @@ export default defineNuxtConfig({
     ],
     app: {
         head: {
-            title: "RustySquare | PvP Rust Servers",
+            title: "RustySquare - PvP Rust Servers",
             meta: [
                 { name: "description", content: "RustySquare PVP Server is a high-performance, action-packed Rust server that offers players a thrilling PVP experience. With a dedicated team of experienced administrators and a variety of custom plugins, RustySquare ensures that players have a fair and enjoyable experience. Our server is optimized for low-latency gameplay and features a variety of custom maps and events to keep things interesting. Whether you're a seasoned veteran or a newcomer to the game, RustySquare is the perfect destination for your Rust PVP needs." }
+            ],
+            link: [
+                {
+                    rel: "shortcut icon", href: "/_nuxt/assets/images/rs_logo.png"
+                }
             ]
         }
     }
