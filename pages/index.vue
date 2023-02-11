b3cb822413cf19fe62cd33568340cff656748f15
Muhammad Ahmer Tahir <82140155+webmentordev@users.noreply.github.com>
2023-02-11 21:53:29 +0500

    added servers

--- pages/index.vue
+++ pages/index.vue
@@ -6,8 +6,9 @@
                 <h1 class="title 645px:text-[8rem] 645px:leading-[8rem] 410px:text-[6rem] 410px:leading-[6rem]">Explore.<br>Build.<br>Survive.</h1>
                 <p class="text-light-gray mb-3 font-semibold text-[18px] leading-8 410px:text-[14px] 410px:leading-5">The only aim in RustySquare is to survive. Environment wants you dead <br> by any mean - the island’s wildlife like Boars, Bears, the environment itself. <br> Do whatever it takes to last another night. <br>Good Night, Good Luck</p>
                 <ul class="flex items-center link">
-                    <NuxtLink class="bg-main text-white text-xl py-2 px-6 mr-3" to="steam://connect/193.29.62.87:38015">2X US</NuxtLink>
-                    <NuxtLink class="bg-main text-white text-xl py-2 px-6" to="steam://connect/193.29.62.87:39015">10X US</NuxtLink>
+                    <NuxtLink class="bg-main text-white text-xl py-2 px-6 mr-3" to="steam://connect/109.205.61.128:38015">2X US</NuxtLink>
+                    <NuxtLink class="bg-main text-white text-xl py-2 px-6 mr-3" to="steam://connect/109.205.61.128:48015">5X US</NuxtLink>
+                    <NuxtLink class="bg-main text-white text-xl py-2 px-6" to="steam://connect/109.205.61.128:39015">10X US</NuxtLink>
                 </ul>
             </div>
         </div>
